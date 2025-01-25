f3b436baf7b7a41bcfa59124dc42a8faa0f3619b
fastasfboiii <ahmedyasser200373@gmail.com>
2025-01-25 10:12:08 +0800

    Update run.py

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ async def start_game(msg):
     await msg.channel.send (f'''Welcome {msg.author.name}, This is Wonder land. in wonder land you can create, raise, sell, buy pets and a lot more...
 let's start with species, you can choose Dragon, Cat, Dog and Bird. each animal have a special sound and they can marry each other to create a new animal
 Dragon can growl, pokemon can create a fire blast, cat can mewo, dog can bark and bird can sing. to create a pet you will need to type:
-!pet create 'your pet's species without the"' 'your pet's name without the"''
+!pet create species gender name 
 REMEMBER you can't give two pet's the same name. be createive you mo**** f*****....sorry that was rude just be createive OK???''')
 
 def pet_name_create (msg):
